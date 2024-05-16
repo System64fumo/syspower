7030c49e88eba60378d0c48d784bd9f123038cca
system64fumo <system64fumo@protonmail.com>
2024-05-17 00:51:18 +0300

    Start killing children
    Somebody call the cops!
    He's gone insane!

--- src/kill.cpp
+++ src/kill.cpp
@@ -2,17 +2,12 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+#include <unistd.h>
 
-using namespace std;
-
-string executeCommand(const string& command) {
-	string result;
+std::string executeCommand(const std::string& command) {
+	std::string result;
 	char buffer[128];
 	FILE* pipe = popen(command.c_str(), "r");
-	if (!pipe) {
-		cerr << "Error: Couldn't open pipe." << endl;
-		return "";
-	}
 	while (!feof(pipe)) {
 		if (fgets(buffer, 128, pipe) != nullptr)
 			result += buffer;
@@ -21,33 +16,41 @@ string executeCommand(const string& command) {
 	return result;
 }
 
-// TODO: Rename this to kill_child_processes,
-// Then start killing every child one by one (Morbid)
-void print_child_processes() {
+void kill_child_processes() {
 	// TODO: Consider adding support for other comps. (Probably won't happen)
-	// Also consider checking if the program exists before running it
-	// Also also maybe consider automatically checking what session the user is
-	// running on?
 
-	string command = "hyprctl clients -j";
-	string output = executeCommand(command);
+	std::string command = "hyprctl clients -j";
+	std::string output = executeCommand(command);
+	pid_t own_pid = getpid();
 
-
-	string searchString = "pid";
+	std::string searchString = "pid";
 	size_t pos = output.find(searchString);
 
 	// Itterate over the output
-	while (pos != string::npos) {
+	while (pos != std::string::npos) {
 		size_t start = output.find(':', pos);
-		if (start != string::npos) {
+		if (start != std::string::npos) {
 			size_t end = output.find_first_of(",}", start);
-			if (end != string::npos) {
-				cout << output.substr(start + 2, end - start - 2) << endl;
-			} else {
-				cerr << "Error: Couldn't find end of the value." << endl;
+			if (end != std::string::npos) {
+				std::string pid = output.substr(start + 2, end - start - 2);
+				std::string kill_command = "hyprctl dispatch closewindow pid:";
+				kill_command += pid;
+
+				// This is not really needed,
+				// Since the program itself is a layershell.
+				// But it might be helpful for when and if i get to adding
+				// support for other compositors
+
+				// Don't kill own process
+				if (std::stoi(pid) == own_pid)
+					continue;
+
+				// TODO: Wait for the program to close
+				// Also change the system status to "Closing xyz..."
+				// And add a button to minimize the shutdown screen to take action
+				// And a button to forcefully kill the program (Skip others too)
+				system(kill_command.c_str());
 			}
-		} else {
-			cerr << "Error: Couldn't find start of the value." << endl;
 		}
 		pos = output.find(searchString, pos + 1);
 	}
