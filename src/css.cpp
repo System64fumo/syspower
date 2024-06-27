5f53a98474c2c754bf1dd7e9de68d576ede636cd
system64fumo <system64fumo@protonmail.com>
2024-06-27 19:31:49 +0300

    Separate css stuff
    Move css stuff to it's own file

---
+++ src/css.cpp
@@ -0,0 +1,13 @@
+#include "css.hpp"
+#include <gtkmm/cssprovider.h>
+#include <filesystem>
+
+css_loader::css_loader(const std::string &path, Gtk::Window *window) {
+	if (!std::filesystem::exists(path))
+		return;
+
+	auto css = Gtk::CssProvider::create();
+	css->load_from_path(path);
+	auto style_context = window->get_style_context();
+	style_context->add_provider_for_display(window->property_display(), css, GTK_STYLE_PROVIDER_PRIORITY_USER);
+}
