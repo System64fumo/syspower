dcd66a424dd99dbb057b6078688c6267b016512c
Devin Droddy <devin.droddy@gmail.com>
2025-01-02 10:56:22 -0500

    tiny indentation thing

--- src/config.hpp
+++ src/config.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Build time configuration		Description
-#define CONFIG_FILE				// Allow the use of a config file
+#define CONFIG_FILE			// Allow the use of a config file
 #define CONFIG_RUNTIME			// Allow the use of runtime arguments
 
 // Default config
